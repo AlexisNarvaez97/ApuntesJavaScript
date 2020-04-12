350ba2beda685f987c6f33e2a2465bd1843ce44f
AlexisNarvaez <alexisnarvaez97@hotmail.com>
2020-04-12 14:03:09 -0500

    Sets y Gets

--- FundamentosJS/classes/class.js
+++ FundamentosJS/classes/class.js
@@ -1,14 +1,23 @@
 
 class Persona {
 
-    nombre;
-    codigo;
-    frase;
+    nombre= '';
+    codigo = '';
+    frase = '';
+    comida = '';
 
     constructor(nombre = 'Sin nombre', codigo = 'Sin codigo', frase = 'Sin frase') {
         this.codigo = codigo;
         this.nombre = nombre;
         this.frase = frase;
+    }
+
+    set setComidaFavorita(comida) {
+        this.comida = comida.toUpperCase();
+    }
+
+    get getComidaFavorita() {
+        return `La comida favorita de ${this.nombre} es ${this.comida}`;
     }
 
     quienSoy() {
@@ -24,8 +33,12 @@ class Persona {
 
 const spiderman = new Persona('Peter Parker', 'Spiderman', 'Hello');
 const ironman = new Persona('Tony Stark', 'IronMan', 'Helouda');
-console.log(spiderman);
 console.log(ironman);
 
 spiderman.miFrase();
-ironman.miFrase();
+// ironman.miFrase();
+
+spiderman.setComidaFavorita = 'El pay de cereza de la tía May';
+
+console.log(spiderman.getComidaFavorita);
+console.log(spiderman);
